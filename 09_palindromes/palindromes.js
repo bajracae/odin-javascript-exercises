951a57a96ca5c998624b88bf3d282560447f71e2
bajracae <aeijanbajracharya@gmail.com>
2025-02-16 19:30:38 -0800

    completed the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+	text = text.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+	let reversed = text.split("").reverse().join("");
+	return text == reversed;
 };
 
 // Do not edit below this line
