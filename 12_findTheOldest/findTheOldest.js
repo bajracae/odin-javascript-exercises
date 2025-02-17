c738b0bf6e283d30cc9361b9727859b7a5ed4923
bajracae <aeijanbajracharya@gmail.com>
2025-02-16 21:10:08 -0800

    completed findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const currentDate = new Date();
+let year = currentDate.getFullYear();
 
+const findTheOldest = function (people) {
+	// let oldestAge = 0;
+	// let oldestPerson;
+	// for (const person of people) {
+	// 	age = !person.yearOfDeath
+	// 		? year - person.yearOfBirth
+	// 		: person.yearOfDeath - person.yearOfBirth;
+
+	// 	if (oldestAge < age) {
+	// 		oldestAge = age;
+	// 		oldestPerson = person;
+	// 	}
+	// }
+	// return oldestPerson;
 };
 
 // Do not edit below this line
